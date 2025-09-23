f944d1212c011908ea5be31f91851555937c04b7
Baboo81 <magicbaboo@gmail.com>
2025-09-23 19:54:04 +0200

    section : accueil

--- resources/data/onepageData.php
+++ resources/data/onepageData.php
@@ -13,6 +13,6 @@ return [
     ],
     'accueil' => [
         'txt' => "Confiez-nous vos extérieurs et profitez d’un jardin toujours impeccable, sans contraintes. De la tonte à la taille, en passant par l’entretien des massifs et le nettoyage saisonnier, nous mettons notre savoir-faire au service de la beauté et de la valeur de votre propriété. Avec un suivi régulier et des prestations adaptées, votre jardin reste net, accueillant et facile à vivre toute l’année.",
-        'img' => "",
+        'img' => "assets/img/accueil/accueilPicto.svg",
     ]
 ];
--- resources/views/onepage.blade.php
+++ resources/views/onepage.blade.php
@@ -11,7 +11,7 @@
 {{-- Anchor : Home --}}
 <a href="" id="#"></a>
 
-{{-- Section : Banner (accueil) --}}
+{{-- Section : Banner --}}
 <section class="banner d-flex align-items-center justify-content-center">
     <div class="container">
         <div class="row">
@@ -25,7 +25,35 @@
         </div>
     </div>
 </section>
-{{-- Section : Banner (acceuil) END --}}
+{{-- Section : Banner END --}}
+
+{{-- Section : Accueil --}}
+<section class="accueil pt-5 pb-5">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-5 my-5">
+                <article class="my-5">
+                    <p class="text-muted">
+                        {{ $data['accueil']['txt'] ?? '' }}
+                    </p>
+                </article>
+            </div>
+            <div class="col-md-7 text-center">
+                <div class="img-fluid">
+                    <img src="{{ $data['accueil']['img'] ?? '' }}" alt="Picto représentant deux jardiniers en pleine activité" id="accueilPicto">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+{{-- Section : Accueil END --}}
+
+{{-- Anchor : Services --}}
+<a href="services"></a>
+
+{{-- Section : Services --}}
+{{-- Section : Services END --}}
+
 
 
 
