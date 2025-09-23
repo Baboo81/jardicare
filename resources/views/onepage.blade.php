4407a86807f20d9d0a4d1f798b7e02de29dd7d77
Baboo81 <magicbaboo@gmail.com>
2025-09-23 14:13:02 +0200

    nav, onepage.blade.php

--- resources/views/onepage.blade.php
+++ resources/views/onepage.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+{{-- Div animation : progression de la page --}}
+<div id="scroll-indicator">
+    <div class="inner-circle">
+        <div class="scroll-arrow">&#x2193;</div>
+    </div>
+</div>
+
+{{-- Anchor : Home --}}
+<a href="" id="#"></a>
+
+{{-- Section : Banner (accueil) --}}
+
 
 @endsection
