ffa5c7a5e888b924eda7371dd40fc83f0cad6208
Baboo81 <magicbaboo@gmail.com>
2025-09-26 17:24:09 +0200

    Ajustements

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -16,15 +16,18 @@
                         <h6> Coordonnées </h6>
                         <p>+32(0)473391930</p>
                         <p>TVA BE 0897 840 512</p>
-                        <a href="#" class="btn-footer"> Contactez-nous</a>
+                        <a href="#contact" class="btn-footer"> Contactez-nous</a>
                     </div>
                 </div>
             </div>
                 <div class="col-md-4 text-center">
                     <div class="row">
                         <div class="col-md-6 px-4">
-                        <h6> Mail </h6>
-                        <p>Contactez-moi par mail en cliquant sur l'enveloppe ci-dessous</p>
+                            <h6> Mail </h6>
+                            <p>Contactez-moi par mail en cliquant sur l'enveloppe ci-dessous</p>
+                            <a href="mailto:odefosseux@jardicare.be">
+                                <img src="assets/img/svg/mail.svg" alt="Icon représentant une enveloppe">
+                            </a>
                         <div class="row ">
                             <div class="col-md-6">
 
